4cba651cfb0717058b4a100e205ae577e3298d69
Hlib Kharytonov <hlibkharytonov@gmai.com>
2026-07-16 23:42:43 +0200

    first touch on renet18

--- test.py
+++ test.py
@@ -1,4 +1,18 @@
 import torch
-print(torch.__version__)
-random_image_size_tensor = torch.rand(size=(224, 224, 3))
-print(random_image_size_tensor.shape, random_image_size_tensor.ndim)
+from torchvision.models import resnet18, ResNet18_Weights
+
+# 1. Отправляем модель на видеокарту
+model = resnet18(weights=ResNet18_Weights.DEFAULT).to('cuda')
+
+# 2. Отправляем фейковую картинку и ответы на видеокарту
+data = torch.rand(1, 3, 64, 64).to('cuda')
+labels = torch.rand(1, 1000).to('cuda')
+
+prediction = model(data)
+loss = (prediction - labels).sum()
+loss.backward()
+
+optim = torch.optim.SGD(model.parameters(), lr=1e-2, momentum=0.9)
+optim.step()
+
+print("Шаг обучения на видеокарте прошел успешно!")
