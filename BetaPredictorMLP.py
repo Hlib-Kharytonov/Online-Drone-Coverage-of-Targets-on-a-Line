26be9d81dbbe82516248a3c2d3ddbc17ecee573f
Hlib KHARYTONOV <hlibkharytonov@gmail.com>
2026-07-20 22:35:40 +0200

    rombic shape of MLP is emplemented, model shows better results

--- BetaPredictorMLP.py
+++ BetaPredictorMLP.py
@@ -3,12 +3,22 @@ import torch.nn as nn
 class BetaPredictorMLP(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(4, 128) 
-        self.fc2 = nn.Linear(128, 128)
-        self.output = nn.Linear(128, 1)
-        self.relu = nn.ReLU()
+        # Распаковка (4 -> 64)
+        self.fc1 = nn.Linear(4, 64) 
+        
+        # Расширение и глубокий анализ (64 -> 128)
+        self.fc2 = nn.Linear(64, 128)
+        
+        # Сжатие и фильтрация шума (128 -> 64)
+        self.fc3 = nn.Linear(128, 64)
+        
+        # Финальный вывод (64 -> 1)
+        self.output = nn.Linear(64, 1)
+        
+        self.activation = nn.ReLU() 
     
     def forward(self, x):
-        x = self.relu(self.fc1(x))
-        x = self.relu(self.fc2(x))
+        x = self.activation(self.fc1(x))
+        x = self.activation(self.fc2(x))
+        x = self.activation(self.fc3(x))
         return self.output(x)
